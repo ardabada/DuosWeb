8afb46ddad18f6a6f25604fae57f57632e6a2f77
ardabada <arlert@mail.ru>
2018-09-27 20:38:35 +0300

    gg

--- MainModule.cs
+++ MainModule.cs
@@ -13,6 +13,19 @@ namespace DuosWeb
         {
             Get["/"] = _ =>
             {
+                System.Web.HttpContext context = System.Web.HttpContext.Current;
+                string ipAddress = context.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+                if (!string.IsNullOrEmpty(ipAddress))
+                {
+                    string[] addresses = ipAddress.Split(',');
+                    if (addresses.Length != 0)
+                    {
+                        return addresses[0];
+                    }
+                }
+
+                return context.Request.ServerVariables["REMOTE_ADDR"];
                 var h = Request.Headers["X-Forwarded-For"];
                 List<string> s = new List<string>(h);
                 string r = s.FirstOrDefault();
