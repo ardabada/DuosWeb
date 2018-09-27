da0349cc2e95beaf0db77fdb55ec3339256741e4
ardabada <arlert@mail.ru>
2018-09-27 19:18:06 +0300

    Initial commit

---
+++ MainModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DuosWeb
+{
+    public class MainModule : Nancy.NancyModule
+    {
+        public MainModule()
+        {
+            Get["/"] = _ =>
+            {
+                string result = "UHA: " + Request.UserHostAddress + "; UA: " + Request.Headers.UserAgent;
+                return result;
+            };
+        }
+    }
+}
