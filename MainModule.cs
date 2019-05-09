c37effa7c4e5c44315b599aa27d9def720df6f89
Alexandr Ivanov <arlert@mail.ru>
2019-05-09 03:12:40 +0300

    tomtom

---
+++ MainModule.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace DuosWeb
+{
+    public class MainModule : NancyModule
+    {
+        public static List<RequestEntry> Data = new List<RequestEntry>();
+
+        public MainModule()
+        {
+            Get["/raw"] = _ =>
+            {
+                return Response.AsText(getRaw());
+            };
+            Post["/raw"] = _ =>
+            {
+                return Response.AsText(getRaw());
+            };
+
+            Get["/tomtom"] = _ =>
+            {
+                Data.Add(new RequestEntry()
+                {
+                    Date = DateTime.Now,
+                    Ip = Request.UserHostAddress + " (" + Request.Headers["X-Forwarded-For"] + ")"
+                });
+
+                string result = "Now: " + DateTime.Now.ToLongTimeString() + "<br>";
+                foreach (var d in Data)
+                {
+                    result += d.Date.ToLongTimeString() + ": " + d.Ip + "<br>";
+                }
+
+                return result;
+            };
+            Get["/tomtom/clear"] = _ =>
+            {
+                Data.Clear();
+                return "cleared";
+            };
+        }
+
+        string getRaw()
+        {
+            var dict = ((DynamicDictionary)Request.Query);
+            string q = string.Join("&", dict.ToDictionary().Select(x => x.Key + "=" + x.Value));
+
+
+            string raw = Request.Method + " " + Request.Path + (string.IsNullOrEmpty(q) ? string.Empty : "?" + q) + " " + Request.ProtocolVersion + Environment.NewLine;
+            raw += Request.UserHostAddress + Environment.NewLine;
+            foreach (var r in Request.Headers)
+            {
+                foreach (var v in r.Value)
+                    raw += r.Key + ": " + v + Environment.NewLine;
+            }
+            raw += Environment.NewLine;
+            raw += new System.IO.StreamReader(Request.Body).ReadToEnd();
+            return raw;
+        }
+    }
+}
---
+++ RequestEntry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DuosWeb
+{
+    public class RequestEntry
+    {
+        public DateTime Date { get; set; }
+        public string Ip { get; set; }
+    }
+}
